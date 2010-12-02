e7465f6a558a822bc7e41abf197302f0762d4b35
netweblogic <netweblogic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-02 22:11:55 +0000

    tagging 3.0.81
    git-svn-id: https://plugins.svn.wordpress.org/events-manager/tags/3.0.81@318222 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/em-locations.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * Static class which will help bulk add/edit/retrieve/manipulate arrays of EM_Location objects. 
+ * Optimized for specifically retreiving locations (whether eventful or not). If you want event data AND location information for each event, use EM_Events
+ * 
+ */
+class EM_Locations extends EM_Object {
+	/**
+	 * Returns an array of EM_Location objects
+	 * @param boolean $eventful
+	 * @param boolean $return_objects
+	 * @return array
+	 */
+	function get( $args = array() ){
+		global $wpdb;
+		$events_table = $wpdb->prefix . EM_EVENTS_TABLE;
+		$locations_table = $wpdb->prefix . EM_LOCATIONS_TABLE;
+		
+		//Quick version, we can accept an array of IDs, which is easy to retrieve
+		if( self::array_is_numeric($args) && count() ){ //Array of numbers, assume they are event IDs to retreive
+			//We can just get all the events here and return them
+			$sql = "SELECT * FROM $locations_table WHERE location_id=".implode(" OR location_id=", $args);
+			$results = $wpdb->get_results($sql);
+			$events = array();
+			foreach($results as $result){
+				$locations[$result['location_id']] = new EM_Location($result);
+			}
+			return $locations; //We return all the events matched as an EM_Event array. 
+		}
+		
+
+		//We assume it's either an empty array or array of search arguments to merge with defaults			
+		$args = self::get_default_search($args);
+		$limit = ( $args['limit'] && is_numeric($args['limit'])) ? "LIMIT {$args['limit']}" : '';
+		$offset = ( $limit != "" && is_numeric($args['offset']) ) ? "OFFSET {$args['offset']}" : '';
+		
+		//Get the default conditions
+		$conditions = self::build_sql_conditions($args);
+		
+		//Put it all together
+		$EM_Location = new EM_Location(0); //Empty class for strict message avoidance
+		$fields = $locations_table .".". implode(", {$locations_table}.", array_keys($EM_Location->fields));
+		$where = ( count($conditions) > 0 ) ? " WHERE " . implode ( " AND ", $conditions ):'';
+		
+		//Get ordering instructions
+		$EM_Event = new EM_Event(); //blank event for below
+		$accepted_fields = $EM_Location->get_fields(true);
+		$accepted_fields = array_merge($EM_Event->get_fields(true),$accepted_fields);
+		$orderby = self::build_sql_orderby($args, $accepted_fields, get_option('dbem_events_default_order'));
+		//Now, build orderby sql
+		$orderby_sql = ( count($orderby) > 0 ) ? 'ORDER BY '. implode(', ', $orderby) : '';
+		
+		
+		//Create the SQL statement and execute
+		$sql = "
+			SELECT $fields FROM $locations_table
+			LEFT JOIN $events_table ON {$locations_table}.location_id={$events_table}.location_id
+			$where
+			GROUP BY location_id
+			$orderby_sql
+			$limit $offset
+		";
+	
+		$results = $wpdb->get_results($sql, ARRAY_A);
+		
+		//If we want results directly in an array, why not have a shortcut here?
+		if( $args['array'] == true ){
+			return $results;
+		}
+		
+		$locations = array();
+		foreach ($results as $location){
+			$locations[] = new EM_Location($location);
+		}
+		return apply_filters('em_locations_get', $locations, $args);
+	}	
+	
+	/**
+	 * Output a set of matched of events
+	 * @param array $args
+	 * @return string
+	 */
+	function output( $args ){
+		global $EM_Location;
+		$EM_Location_old = $EM_Location; //When looping, we can replace EM_Location global with the current event in the loop
+		//Can be either an array for the get search or an array of EM_Location objects
+		if( is_object(current($args)) && get_class((current($args))) == 'EM_Location' ){
+			$locations = $args;
+		}else{
+			$locations = self::get( $args );
+		}
+		//What format shall we output this to, or use default
+		$format = ( $args['format'] == '' ) ? get_option( 'dbem_location_list_item_format' ) : $args['format'] ;
+		
+		$output = "";
+		if ( count($locations) > 0 ) {
+			foreach ( $locations as $location ) {
+				$EM_Location = $location;
+				/* @var EM_Event $event */
+				$output .= $location->output($format);
+			}
+			//Add headers and footers to output
+			if( $format == get_option ( 'dbem_location_list_item_format' ) ){
+				$single_event_format_header = get_option ( 'dbem_location_list_item_format_header' );
+				$single_event_format_header = ( $single_event_format_header != '' ) ? $single_event_format_header : "<ul class='dbem_events_list'>";
+				$single_event_format_footer = get_option ( 'dbem_location_list_item_format_footer' );
+				$single_event_format_footer = ( $single_event_format_footer != '' ) ? $single_event_format_footer : "</ul>";
+				$output =  $single_event_format_header .  $output . $single_event_format_footer;
+			}
+			//TODO pagination for locations
+		} else {
+			$output = get_option ( 'dbem_no_events_message' );
+		}
+		//FIXME check if reference is ok when restoring object, due to changes in php5 v 4
+		$EM_Location_old= $EM_Location;
+		return apply_filters('em_locations_output', $output, $locations, $args);		
+	}
+	
+	/**
+	 * Builds an array of SQL query conditions based on regularly used arguments
+	 * @param array $args
+	 * @return array
+	 */
+	function build_sql_conditions( $args = array() ){
+		global $wpdb;
+		$events_table = $wpdb->prefix . EM_EVENTS_TABLE;
+		$locations_table = $wpdb->prefix . EM_LOCATIONS_TABLE;
+		
+		$conditions = parent::build_sql_conditions($args);
+		//eventful locations
+		if( true == $args['eventful'] ){
+			$conditions['eventful'] = "{$events_table}.event_id IS NOT NULL";
+		}elseif( true == $args['eventless'] ){
+			$conditions['eventless'] = "{$events_table}.event_id IS NULL";
+		}
+		return apply_filters('em_locations_build_sql_conditions', $conditions, $args);
+	}
+	
+	/* Overrides EM_Object method to apply a filter to result
+	 * @see wp-content/plugins/events-manager/classes/EM_Object#build_sql_orderby()
+	 */
+	function build_sql_orderby( $args, $accepted_fields, $default_order = 'ASC' ){
+		return apply_filters( 'em_locations_build_sql_orderby', parent::build_sql_orderby($args, $accepted_fields, get_option('dbem_events_default_order')), $args, $accepted_fields, $default_order );
+	}
+	
+	/* 
+	 * Generate a search arguments array from defalut and user-defined.
+	 * @see wp-content/plugins/events-manager/classes/EM_Object::get_default_search()
+	 */
+	function get_default_search($array = array()){
+		$defaults = array(
+			'eventful' => false, //Locations that have an event (scope will also play a part here
+			'eventless' => false, //Locations WITHOUT events, eventful takes precedence
+			'orderby' => 'name',
+			'scope' => 'all' //we probably want to search all locations by default, not like events
+		);
+		$array['eventful'] = ( !empty($array['eventful']) && $array['eventful'] == true );
+		$array['eventless'] = ( !empty($array['eventless']) && $array['eventless'] == true );
+		return apply_filters('em_locations_get_default_search', parent::get_default_search($defaults, $array), $array, $defaults);
+	}
+	//TODO for all the static plural classes like this one, we might benefit from bulk actions like delete/add/save etc.... just a random thought.
+}
+?>
---
+++ em-functions.php
@@ -0,0 +1,199 @@
+<?php
+function em_hello_to_new_user() {
+	if ( get_option ( 'dbem_hello_to_user' ) == 1 ) {
+		$current_user = wp_get_current_user ();
+		$advice = sprintf ( __ ( "<p>Hey, <strong>%s</strong>, welcome to <strong>Events Manager</strong>! We hope you like it around here.</p> 
+		<p>Now it's time to insert events lists through  <a href='%s' title='Widgets page'>widgets</a>, <a href='%s' title='Template tags documentation'>template tags</a> or <a href='%s' title='Shortcodes documentation'>shortcodes</a>.</p>
+		<p>By the way, have you taken a look at the <a href='%s' title='Change settings'>Settings page</a>? That's where you customize the way events and locations are displayed.</p>
+		<p>What? Tired of seeing this advice? I hear you, <a href='%s' title='Don't show this advice again'>click here</a> and you won't see this again!</p>", 'dbem' ), $current_user->display_name, get_bloginfo ( 'url' ) . '/wp-admin/widgets.php', 'http://wp-events-plugin.com/documentation/template-tags/', 'http://wp-events-plugin.com/documentation/shortcodes/', get_bloginfo ( 'url' ) . '/wp-admin/admin.php?page=events-manager-options', get_bloginfo ( 'url' ) . '/wp-admin/admin.php?page=events-manager/events-manager.php&disable_hello_to_user=true' );
+		?>
+		<div id="message" class="updated">
+			<?php echo $advice; ?>
+		</div>
+		<?php
+	}
+}
+
+/**
+ * Takes a few params and determins a pagination link structure
+ * @param string $link
+ * @param int $total
+ * @param int $limit
+ * @param int $page
+ * @param int $pagesToShow
+ * @return string
+ */
+function em_paginate($link, $total, $limit, $page=1, $pagesToShow=10){
+	if($limit > 0){
+		$maxPages = ceil($total/$limit); //Total number of pages
+		$startPage = ($page <= $pagesToShow) ? 1 : $pagesToShow * (floor($page/$pagesToShow)) ; //Which page to start the pagination links from (in case we're on say page 12 and $pagesToShow is 10 pages)
+		$placeholder = urlencode('%PAGE%');
+		$link = str_replace('%PAGE%', urlencode('%PAGE%'), $link); //To avoid url encoded/non encoded placeholders
+	    //Add the back and first buttons
+		    $string = ($page>1) ? '<a href="'.str_replace($placeholder,1,$link).'">&lt;&lt;</a> ' : '&lt;&lt; ';
+		    $string .= ($page>1) ? ' <a href="'.str_replace($placeholder,$page-1,$link).'">&lt;</a> ' : '&lt; ';
+		//Loop each page and create a link or just a bold number if its the current page
+		    for ($i = $startPage ; $i < $startPage+$pagesToShow && $i <= $maxPages ; $i++){
+	            if($i == $page){
+	                $string .= " <strong>$i</strong>";
+	            }else{
+	                $string .= ' <a href="'.str_replace($placeholder,$i,$link).'">'.$i.'</a> ';                
+	            }
+		    }
+		//Add the forward and last buttons
+		    $string .= ($i < $maxPages) ? ' <a href="'.str_replace($placeholder,$page+1,$link).'">&gt;</a> ' :' &gt; ' ;
+		    $string .= ($i < $maxPages) ? ' <a href="'.str_replace($placeholder,$maxPages,$link).'">&gt;&gt;</a> ' : '&gt;&gt; ';
+		//Return the string
+		    return $string;
+	}
+}
+
+/**
+ * Takes a url and appends GET params (supplied as an assoc array), it automatically detects if you already have a querystring there
+ * @param string $url
+ * @param array $params
+ */
+function em_add_get_params($url, $params=array()){
+	$has_querystring = (stristr($url, "?"));	
+	$count = 0;
+	foreach($params as $key=>$value){
+		if( $count == 0 && !$has_querystring ){
+			$url .= "?{$key}=".urlencode($value);
+		}else{
+			$url .= "&amp;{$key}=".urlencode($value);
+		}
+		$count++;
+	}
+	return $url;
+}
+
+function url_exists($url) {	
+	if ((strpos ( $url, "http" )) === false)
+		$url = "http://" . $url;
+		// FIXME ripristina la linea seguente e VEDI DI SISTEMARE!!!!
+	// if (is_array(@get_headers($url))) {
+	if (true)
+		return true;
+	else
+		return false;
+}
+
+/**
+ * Gets all WP users
+ * @return array
+ */
+function em_get_wp_users() {
+	global $wpdb;
+	$sql = "SELECT display_name, ID FROM $wpdb->users";  
+	$users = $wpdb->get_results($sql, ARRAY_A);  
+	$indexed_users = array();
+	foreach($users as $user) 
+		$indexed_users[$user['ID']] = $user['display_name'];
+ 	return $indexed_users;
+}
+
+/*
+ * UI Helpers
+ * previously dbem_UI_helpers.php functions
+ */
+
+function em_option_items($array, $saved_value) {
+	$output = "";
+	foreach($array as $key => $item) {    
+		$selected ='';
+		if ($key == $saved_value)
+			$selected = "selected='selected'";
+		$output .= "<option value='$key' $selected >$item</option>\n";
+	
+	} 
+	echo $output;
+}
+
+function em_checkbox_items($name, $array, $saved_values, $horizontal = true) { 
+	$output = "";
+	foreach($array as $key => $item) {
+		$checked = "";
+		if (in_array($key, $saved_values))
+			$checked = "checked='checked'";  
+		$output .=  "<input type='checkbox' name='$name' value='$key' $checked /> $item ";
+		if(!$horizontal)	
+			$output .= "<br/>\n";
+	}
+	echo $output;
+	
+}
+
+function em_options_input_text($title, $name, $description) {
+	?>
+	<tr valign="top" id='<?php echo $name;?>_row'>
+		<th scope="row"><?php _e($title, 'dbem') ?></th>
+	    <td>
+			<input name="<?php echo $name ?>" type="text" id="<?php echo $title ?>" style="width: 95%" value="<?php echo htmlspecialchars(get_option($name), ENT_QUOTES); ?>" size="45" /><br />
+						<?php _e($description, 'dbem') ?>
+			</td>
+		</tr>
+	<?php
+}
+function em_options_input_password($title, $name, $description) {
+	?>
+	<tr valign="top" id='<?php echo $name;?>_row'>
+		<th scope="row"><?php _e($title, 'dbem') ?></th>
+	    <td>
+			<input name="<?php echo $name ?>" type="password" id="<?php echo $title ?>" style="width: 95%" value="<?php echo get_option($name); ?>" size="45" /><br />
+						<?php echo $description; ?>
+			</td>
+		</tr>
+	<?php
+}
+
+function em_options_textarea($title, $name, $description) {
+	?>
+	<tr valign="top" id='<?php echo $name;?>_row'>
+		<th scope="row"><?php _e($title,'dbem')?></th>
+			<td><textarea name="<?php echo $name ?>" id="<?php echo $name ?>" rows="6" cols="60"><?php echo htmlspecialchars(get_option($name), ENT_QUOTES);?></textarea><br/>
+				<?php echo $description; ?></td>
+		</tr>
+	<?php
+}
+
+function em_options_radio_binary($title, $name, $description) {
+		$list_events_page = get_option($name); ?>
+		 
+	   	<tr valign="top" id='<?php echo $name;?>_row'>
+	   		<th scope="row"><?php _e($title,'dbem'); ?></th>
+	   		<td>   
+				<input id="<?php echo $name ?>_yes" name="<?php echo $name ?>" type="radio" value="1" <?php if($list_events_page) echo "checked='checked'"; ?> /><?php _e('Yes'); ?> <br />
+				<input  id="<?php echo $name ?>_no" name="<?php echo $name ?>" type="radio" value="0" <?php if(!$list_events_page) echo "checked='checked'"; ?> /><?php _e('No'); ?> <br />
+				<?php echo $description; ?>
+			</td>
+	   	</tr>
+<?php	
+}  
+function em_options_select($title, $name, $list, $description) {
+	$option_value = get_option($name);
+	if( $name == 'dbem_events_page' && !is_object(get_page($option_value)) ){
+		$option_value = 0; //Special value
+	}
+	?>
+   	<tr valign="top" id='<?php echo $name;?>_row'>
+   		<th scope="row"><?php _e($title,'dbem'); ?></th>
+   		<td>   
+			<select name="<?php echo $name; ?>" > 
+				<?php foreach($list as $key => $value) : ?>   
+ 				<option value='<?php echo $key ?>' <?php echo ("$key" == $option_value) ? "selected='selected' " : ''; ?>>
+ 					<?php echo $value; ?>
+ 				</option>
+				<?php endforeach; ?>
+			</select> <br/>
+			<?php echo $description; ?>
+		</td>
+   	</tr>
+	<?php	
+}
+// got from http://davidwalsh.name/php-email-encode-prevent-spam
+function em_ascii_encode($e){  
+	$output = '';
+    for ($i = 0; $i < strlen($e); $i++) { $output .= '&#'.ord($e[$i]).';'; }  
+    return $output;  
+}
+?>
