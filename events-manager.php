1fb1810e549650f293a718de61cb6d0cfb8bec2c
netweblogic <netweblogic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-05 00:10:27 +0000

    git-svn-id: https://plugins.svn.wordpress.org/events-manager/tags/3.0.93@340869 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- events-manager.php
+++ events-manager.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Events Manager
-Version: 3.0.92
+Version: 3.0.93
 Plugin URI: http://wp-events-plugin.com
 Description: Manage events specifying precise spatial data (Location, Town, Province, etc).
 Author: Davide Benini, Marcus Sykes
