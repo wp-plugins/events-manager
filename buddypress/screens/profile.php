fb9140d1a20c838c8ab698f4a6821e719b86b57b
netweblogic <netweblogic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-16 21:19:14 +0000

    Tagging 4.14
    git-svn-id: https://plugins.svn.wordpress.org/events-manager/tags/4.14@424463 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- buddypress/screens/profile.php
+++ buddypress/screens/profile.php
@@ -30,25 +30,40 @@ function bp_em_events() {
 	function bp_em_events_content() {
 		global $bp, $EM_Notices;
 		echo $EM_Notices;
-		?>
-		<h4><?php _e('My Events', 'dbem'); ?></h4>
-		<?php
-		$events = EM_Events::get(array('owner'=>$bp->displayed_user->id));
-		if( count($events) > 0 ){
-			$args = array(
-				'format_header' => get_option('dbem_bp_events_list_format_header'),
-				'format' => get_option('dbem_bp_events_list_format'),
-				'format_footer' => get_option('dbem_bp_events_list_format_footer'),
-				'owner' => $bp->displayed_user->id
-			);
-			echo EM_Events::output($events, $args);
-		}else{
+		if( user_can($bp->displayed_user->id,'edit_events') ){
 			?>
-			<p><?php _e('No Events', 'dbem'); ?></p>
+			<h4><?php _e('My Events', 'dbem'); ?></h4>
 			<?php
+			$events = EM_Events::get(array('owner'=>$bp->displayed_user->id));
+			if( count($events) > 0 ){
+				$args = array(
+					'format_header' => get_option('dbem_bp_events_list_format_header'),
+					'format' => get_option('dbem_bp_events_list_format'),
+					'format_footer' => get_option('dbem_bp_events_list_format_footer'),
+					'owner' => $bp->displayed_user->id
+				);
+				echo EM_Events::output($events, $args);
+			}else{
+				?>
+				<p><?php _e('No Events', 'dbem'); ?></p>
+				<?php
+			}
 		}
 		?>
 		<h4><?php _e("Events I'm Attending", 'dbem'); ?></h4>
 		<?php
-		bp_em_attending_content();
+		$EM_Person = new EM_Person( $bp->displayed_user->id );
+		$EM_Bookings = $EM_Person->get_bookings();
+		if(count($EM_Bookings->bookings) > 0){
+			//Get events here in one query to speed things up
+			$event_ids = array();
+			foreach($EM_Bookings as $EM_Booking){
+				$event_ids[] = $EM_Booking->event_id;
+			}
+			echo EM_Events::output(array('event'=>$event_ids));
+		}else{
+			?>
+			<p><?php _e('Not attending any events yet.','dbem'); ?></p>
+			<?php
+		}
 	}
