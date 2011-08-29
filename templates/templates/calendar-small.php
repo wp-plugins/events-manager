178e54762df2df94bec74aa23541066d3d0d96e0
netweblogic <netweblogic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-29 08:05:50 +0000

    small calendar eventful fix
    git-svn-id: https://plugins.svn.wordpress.org/events-manager/trunk@430008 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/templates/calendar-small.php
+++ templates/templates/calendar-small.php
@@ -33,7 +33,7 @@
 				}
 				?>
 				<td class="<?php echo $class; ?>">
-					<?php if( $class == 'eventful' ): ?>
+					<?php if( !empty($cell_data['events']) && count($cell_data['events']) > 0 ): ?>
 					<a href="<?php echo $cell_data['link']; ?>" title="<?php echo $cell_data['link_title']; ?>"><?php echo date('j',$cell_data['date']); ?></a>
 					<?php else:?>
 					<?php echo date('j',$cell_data['date']); ?>
