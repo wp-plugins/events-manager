6c00a2e5f5199cbab2ddd99d31860f96c9cc639e
netweblogic <netweblogic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-23 18:44:54 +0000

    git-svn-id: https://plugins.svn.wordpress.org/events-manager/tags/5.0.51@494129 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- em-functions.php
+++ em-functions.php
@@ -37,9 +37,9 @@ function em_paginate($link, $total, $limit, $page=1, $pagesToShow=10){
 	            if($i == $page){
 	                $string .= ' <strong><span class="page-numbers current">'.$i.'</span></strong>';
 	            }elseif($i=='1'){
-	                $string .= ' <a class="page-numbers" href="'.$base_link.$base_querystring.'">'.$i.'</a> ';                
+	                $string .= ' <a class="page-numbers" href="'.$base_link.$base_querystring.'">'.$i.'</a> ';
 	            }else{
-	                $string .= ' <a class="page-numbers" href="'.str_replace($placeholder,$i,$link).'">'.$i.'</a> ';                
+	                $string .= ' <a class="page-numbers" href="'.str_replace($placeholder,$i,$link).'">'.$i.'</a> ';
 	            }
 		    }
 		//Add the forward and last buttons
@@ -62,7 +62,7 @@ function em_paginate($link, $total, $limit, $page=1, $pagesToShow=10){
  * @uses paginate_links()
  * @uses add_query_arg()
  */
-function em_admin_paginate($total, $limit, $page=1, $vars=false, $base = false, $format = ''){				
+function em_admin_paginate($total, $limit, $page=1, $vars=false, $base = false, $format = ''){
 	$return = '<div class="tablenav-pages">';
 	$base = !empty($base) ? $base:add_query_arg( 'pno', '%#%' );
 	$events_nav = paginate_links( array(
@@ -90,7 +90,7 @@ function em_admin_paginate($total, $limit, $page=1, $vars=false, $base = false, 
  * @param bool $encode
  * @return string
  */
-function em_add_get_params($url, $params=array(), $html=true, $encode=true){	
+function em_add_get_params($url, $params=array(), $html=true, $encode=true){
 	//Splig the url up to get the params and the page location
 	$url_parts = explode('?', $url);
 	$url = $url_parts[0];
@@ -102,7 +102,7 @@ function em_add_get_params($url, $params=array(), $html=true, $encode=true){
 			if( strstr($url_params_dirty, '&amp;') !== false ){
 				$url_params_dirty = explode('&amp;', $url_params_dirty);
 			}else{
-				$url_params_dirty = explode('&', $url_params_dirty);		
+				$url_params_dirty = explode('&', $url_params_dirty);
 			}
 		}
 		//split further into associative array
@@ -156,7 +156,7 @@ function em_get_countries($add_blank = false){
 }
 
 /**
- * Returns an array of scopes available to events manager. Hooking into this function's em_get_scopes filter will allow you to add scope options to the event pages. 
+ * Returns an array of scopes available to events manager. Hooking into this function's em_get_scopes filter will allow you to add scope options to the event pages.
  */
 function em_get_scopes(){
 	$scopes = array(
@@ -180,7 +180,7 @@ function em_get_currencies(){
 	$currencies = new stdClass();
 	$currencies->names = array('EUR' => 'EUR - Euros','USD' => 'USD - U.S. Dollars','GBP' => 'GBP - British Pounds','CAD' => 'CAD - Canadian Dollars','AUD' => 'AUD - Australian Dollars','BRL' => 'BRL - Brazilian Reais','CZK' => 'CZK - Czech Koruny','DKK' => 'DKK - Danish Kroner','HKD' => 'HKD - Hong Kong Dollars','HUF' => 'HUF - Hungarian Forints','ILS' => 'ILS - Israeli New Shekels','JPY' => 'JPY - Japanese Yen','MYR' => 'MYR - Malaysian Ringgit','MXN' => 'MXN - Mexican Pesos','TWD' => 'TWD - New Taiwan Dollars','NZD' => 'NZD - New Zealand Dollars','NOK' => 'NOK - Norwegian Kroner','PHP' => 'PHP - Philippine Pesos','PLN' => 'PLN - Polish Zlotys','SGD' => 'SGD - Singapore Dollars','SEK' => 'SEK - Swedish Kronor','CHF' => 'CHF - Swiss Francs','THB' => 'THB - Thai Baht','TRY' => 'TRY - Turkish Liras');
 	$currencies->symbols = array( 'EUR' => '&euro;','USD' => '$','GBP' => '&pound;','CAD' => '$','AUD' => '$','BRL' => 'R$','DKK' => 'kr','HKD' => '$','HUF' => 'Ft','JPY' => '&#165;','MYR' => 'RM','MXN' => '$','TWD' => '$','NZD' => '$','NOK' => 'kr','PHP' => 'Php','SGD' => '$','SEK' => 'kr','CHF' => 'CHF','TRY' => 'TL');
-	$currencies->true_symbols = array( 'EUR' => '€','USD' => '$','GBP' => '£','CAD' => '$','AUD' => '$','BRL' => 'R$','DKK' => 'kr','HKD' => '$','HUF' => 'Ft','JPY' => '¥','MYR' => 'RM','MXN' => '$','TWD' => '$','NZD' => '$','NOK' => 'kr','PHP' => 'Php','SGD' => '$','SEK' => 'kr','CHF' => 'CHF','TRY' => 'TL');
+	$currencies->true_symbols = array( 'EUR' => 'â‚¬','USD' => '$','GBP' => 'Â£','CAD' => '$','AUD' => '$','BRL' => 'R$','DKK' => 'kr','HKD' => '$','HUF' => 'Ft','JPY' => 'Â¥','MYR' => 'RM','MXN' => '$','TWD' => '$','NZD' => '$','NOK' => 'kr','PHP' => 'Php','SGD' => '$','SEK' => 'kr','CHF' => 'CHF','TRY' => 'TL');
 	return apply_filters('em_get_currencies',$currencies);
 }
 
@@ -233,7 +233,7 @@ function em_get_days_names(){
  */
 function em_verify_nonce($action, $nonce_name='_wpnonce'){
 	if( is_admin() ){
-		if( !wp_verify_nonce($_REQUEST[$nonce_name] && $action) ) check_admin_referer('trigger_error');				
+		if( !wp_verify_nonce($_REQUEST[$nonce_name] && $action) ) check_admin_referer('trigger_error');
 	}else{
 		if( !wp_verify_nonce($_REQUEST[$nonce_name] && $action) ) exit( __('Trying to perform an illegal action.','dbem') );
 	}
@@ -255,8 +255,9 @@ function em_get_wp_users( $args = array(), $extra_users = array() ) {
 
 function em_get_attributes($lattributes = false){
 	//We also get a list of attribute names and create a ddm list (since placeholders are fixed)
-	$formats = 
+	$formats =
 		get_option ( 'dbem_placeholders_custom' ).
+		get_option ( 'dbem_location_placeholders_custom' ).
 		get_option ( 'dbem_event_list_item_format' ).
 		get_option ( 'dbem_event_page_title_format' ).
 		get_option ( 'dbem_full_calendar_event_format' ).
@@ -277,7 +278,7 @@ function em_get_attributes($lattributes = false){
 	//Now grab all the unique attributes we can use in our event.
 	$attributes = array('names'=>array(), 'values'=>array());
 	foreach($matches[1] as $key => $attribute) {
-		if( !in_array($attribute, $attributes['names']) ){			
+		if( !in_array($attribute, $attributes['names']) ){
 			$attributes['names'][] = $attribute ;
 			//check if there's ddm values
 			$attribute_values = array();
@@ -291,7 +292,7 @@ function em_get_attributes($lattributes = false){
 }
 
 /**
- * Copied straight from wp-login.php, only change atm is a function renaming. 
+ * Copied straight from wp-login.php, only change atm is a function renaming.
  * Handles registering a new user.
  *
  * @param array associative array of user values to insert
@@ -339,7 +340,7 @@ function em_register_new_user( $user_data ) {
 		return $errors;
 
 	$user_data['user_pass'] = wp_generate_password( 12, false);
-	
+
 	$user_id = wp_insert_user( $user_data );
 	if( is_numeric($user_id) && !empty($user_data['dbem_phone']) ){
 		update_user_meta($user_id, 'dbem_phone', $user_data['dbem_phone']);
@@ -367,16 +368,16 @@ function em_register_new_user( $user_data ) {
  */
 function em_new_user_notification($user_id, $plaintext_pass = '') {
 	global $LoginWithAjax;
-	
+
 	//if you want you can disable this email from going out, and will still consider registration as successful.
 	if( get_option('dbem_email_disable_registration') ){ return true;  }
-	
+
 	//Copied out of /wp-includes/pluggable.php
 	$user = new WP_User($user_id);
 
 	$user_login = stripslashes($user->user_login);
 	$user_email = stripslashes($user->user_email);
-	
+
 	// The blogname option is escaped with esc_html on the way into the database in sanitize_option
 	// we want to reverse this for the plain text arena of emails.
 	$blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
@@ -388,13 +389,13 @@ function em_new_user_notification($user_id, $plaintext_pass = '') {
 
 	if ( empty($plaintext_pass) )
 		return;
-			
+
 	//
 	ob_start();
-	em_locate_template('emails/new-user.php', true);	
+	em_locate_template('emails/new-user.php', true);
 	$message = ob_get_clean();
 	$message  = str_replace(array('%password%','%username%'), array($plaintext_pass, $user_login), $message);
-	
+
 	return wp_mail($user_email, sprintf(__('[%s] Your username and password', 'dbem'), $blogname), $message);
 }
 
@@ -405,28 +406,28 @@ function em_new_user_notification($user_id, $plaintext_pass = '') {
 
 function em_option_items($array, $saved_value) {
 	$output = "";
-	foreach($array as $key => $item) {    
+	foreach($array as $key => $item) {
 		$selected ='';
 		if ($key == $saved_value)
 			$selected = "selected='selected'";
 		$output .= "<option value='".esc_attr($key)."' $selected >".esc_html($item)."</option>\n";
-	
-	} 
+
+	}
 	echo $output;
 }
 
-function em_checkbox_items($name, $array, $saved_values, $horizontal = true) { 
+function em_checkbox_items($name, $array, $saved_values, $horizontal = true) {
 	$output = "";
 	foreach($array as $key => $item) {
 		$checked = "";
 		if (in_array($key, $saved_values))
-			$checked = "checked='checked'";  
+			$checked = "checked='checked'";
 		$output .=  "<input type='checkbox' name='".esc_attr($name)."' value='".esc_attr($key)."' $checked /> ".esc_html($item);
-		if(!$horizontal)	
+		if(!$horizontal)
 			$output .= "<br/>\n";
 	}
 	echo $output;
-	
+
 }
 
 function em_options_input_text($title, $name, $description, $default='') {
@@ -465,15 +466,15 @@ function em_options_textarea($title, $name, $description) {
 }
 
 function em_options_radio($name, $options, $title='') {
-		$option = get_option($name); 
-		?>		 
+		$option = get_option($name);
+		?>
 	   	<tr valign="top" id='<?php echo esc_attr($name);?>_row'>
 	   		<?php if( !empty($title) ): ?>
 	   		<th scope="row"><?php  echo esc_html($title); ?></th>
 	   		<td>
 	   		<?php else: ?>
 	   		<td colspan="2">
-	   		<?php endif; ?>  
+	   		<?php endif; ?>
 	   			<table>
 	   			<?php foreach($options as $value => $text): ?>
 	   				<tr>
@@ -484,27 +485,27 @@ function em_options_radio($name, $options, $title='') {
 				</table>
 			</td>
 	   	</tr>
-<?php	
+<?php
 }
 
 function em_options_radio_binary($title, $name, $description, $option_names = '') {
 	if( empty($option_names) ) $option_names = array(0 => __('No','dbem'), 1 => __('Yes','dbem'));
 	if( substr($name, 0, 7) == 'dbem_ms' ){
-		$list_events_page = get_site_option($name); 
+		$list_events_page = get_site_option($name);
 	}else{
-		$list_events_page = get_option($name); 
+		$list_events_page = get_option($name);
 	}
-	?>		 
+	?>
    	<tr valign="top" id='<?php echo $name;?>_row'>
    		<th scope="row"><?php echo esc_html($title); ?></th>
-   		<td>  
+   		<td>
    			<?php echo $option_names[1]; ?> <input id="<?php echo esc_attr($name) ?>_yes" name="<?php echo esc_attr($name) ?>" type="radio" value="1" <?php if($list_events_page) echo "checked='checked'"; ?> />&nbsp;&nbsp;&nbsp;
 			<?php echo $option_names[0]; ?> <input  id="<?php echo esc_attr($name) ?>_no" name="<?php echo esc_attr($name) ?>" type="radio" value="0" <?php if(!$list_events_page) echo "checked='checked'"; ?> />
 			<br/><em><?php echo $description; ?></em>
 		</td>
    	</tr>
-	<?php	
-}  
+	<?php
+}
 
 function em_options_select($title, $name, $list, $description) {
 	$option_value = get_option($name);
@@ -514,9 +515,9 @@ function em_options_select($title, $name, $list, $description) {
 	?>
    	<tr valign="top" id='<?php echo esc_attr($name);?>_row'>
    		<th scope="row"><?php echo esc_html($title); ?></th>
-   		<td>   
-			<select name="<?php echo esc_attr($name); ?>" > 
-				<?php foreach($list as $key => $value) : ?>   
+   		<td>
+			<select name="<?php echo esc_attr($name); ?>" >
+				<?php foreach($list as $key => $value) : ?>
  				<option value='<?php echo esc_attr($key) ?>' <?php echo ("$key" == $option_value) ? "selected='selected' " : ''; ?>>
  					<?php echo esc_html($value); ?>
  				</option>
@@ -525,13 +526,13 @@ function em_options_select($title, $name, $list, $description) {
 			<em><?php echo $description; ?></em>
 		</td>
    	</tr>
-	<?php	
+	<?php
 }
 // got from http://davidwalsh.name/php-email-encode-prevent-spam
-function em_ascii_encode($e){  
+function em_ascii_encode($e){
 	$output = '';
-    for ($i = 0; $i < strlen($e); $i++) { $output .= '&#'.ord($e[$i]).';'; }  
-    return $output;  
+    for ($i = 0; $i < strlen($e); $i++) { $output .= '&#'.ord($e[$i]).';'; }
+    return $output;
 }
 
 
@@ -540,7 +541,7 @@ if( !function_exists('get_current_blog_id') ){
 	 * Substitutes the original function in 3.1 onwards, for backwards compatability (only created if not previously defined)
 	 * @return int
 	 */
-	function get_current_blog_id(){ return 1; } //for < 3.1 
+	function get_current_blog_id(){ return 1; } //for < 3.1
 }
 
 function em_get_thumbnail_url($image_url, $width, $height){
