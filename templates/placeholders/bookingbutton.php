81ec846b123cc4f4516a656eccacc1c9bcd9ac6e
netweblogic <netweblogic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-18 16:33:40 +0000

    tagging 5.2.5
    git-svn-id: https://plugins.svn.wordpress.org/events-manager/tags/5.2.5@600626 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/placeholders/bookingbutton.php
+++ templates/placeholders/bookingbutton.php
@@ -1,43 +1,32 @@
-<?php 
-$button_text = __('Book Now', 'dbem');
-$button_booking = __('Booking...','dbem');
-$button_success = __('Booking Submitted','dbem');
-$button_fail = __('Booking Error. Try again?','dbem');
+<?php
+/*
+ * You can override this by copying this file to wp-content/themesyourthemefolder/plugins/events-manager/placeholders/ and modifying it however you need.
+ * There are a few variables made available to you:
+ * 
+ * $EM_Event - EM_Event object 
+ */
+$notice_full = get_option('dbem_booking_button_msg_full');
+$button_text = get_option('dbem_booking_button_msg_book');
+$button_booking = get_option('dbem_booking_button_msg_booking');
+$button_success = get_option('dbem_booking_button_msg_booked');
+$button_fail = get_option('dbem_booking_button_msg_error');
+$button_cancel = get_option('dbem_booking_button_msg_cancel');
+$button_canceling = get_option('dbem_booking_button_msg_canceling');
+$button_cancel_success = get_option('dbem_booking_button_msg_cancelled');
+$button_cancel_fail = get_option('dbem_booking_button_msg_cancel_error');
+/* @var $EM_Event EM_Event */
 ?>
-<?php if( count($EM_Event->get_bookings()->get_available_tickets()->tickets) == 1 && is_user_logged_in() ):  ?>
-	<?php ob_start(); ?>
-	<a id="em-booking-button-<?php echo $EM_Event->id; ?>" class="button"><?php echo $button_text; ?></a>
+<?php if( $EM_Event->get_bookings()->is_open() && is_user_logged_in() ):  ?>
+	<?php
+		$EM_Booking = $EM_Event->get_bookings()->has_booking(); 
+		ob_start(); 
+	?>
+	<?php if( is_object($EM_Booking) && !get_option('dbem_bookings_double') && $EM_Booking->status != 3 && get_option('dbem_bookings_user_cancellation') ) : $show_js = true; ?>
+		<a id="em-cancel-button_<?php echo $EM_Booking->booking_id; ?>_<?php echo wp_create_nonce('booking_cancel'); ?>" class="button em-cancel-button"><?php echo $button_cancel; ?></a>
+	<?php elseif( !is_object($EM_Booking) || get_option('dbem_bookings_double') ) : $show_js = true; ?>
+		<a id="em-booking-button_<?php echo $EM_Event->event_id; ?>_<?php echo wp_create_nonce('booking_add_one'); ?>" class="button em-booking-button"><?php echo $button_text; ?></a>
+	<?php endif; ?>	
 	<?php echo apply_filters( 'em_booking_button', ob_get_clean(), $EM_Event ); ?>
-	<?php ob_start(); ?>
-	<script type="text/javascript">
-		jQuery(document).ready( function($){
-			$('#em-booking-button-<?php echo $EM_Event->id; ?>').click(function(){
-				if( $(this).text() != '<?php echo $button_success; ?>' && $(this).text() != '<?php echo $button_booking; ?>'){
-					$(this).text('<?php echo $button_booking; ?>');
-					$.ajax({
-						url: EM.ajaxurl,
-						dataType: 'jsonp',
-						beforeSubmit: function(formData, jqForm, options) {
-							$('.em-booking-message').remove();
-							$('#em-booking-form').append('<div id="em-loading"></div>');
-						},
-						data: {
-							event_id : '<?php echo $EM_Event->id; ?>',
-							_wpnonce : '<?php echo wp_create_nonce('booking_add_one') ?>',
-							action : 'booking_add_one'
-						},
-						success : function(response, statusText, xhr, $form) {
-							if(response.result){
-								$('#em-booking-button-<?php echo $EM_Event->id; ?>').text('<?php echo $button_success; ?>');
-							}else{
-								$('#em-booking-button-<?php echo $EM_Event->id; ?>').text('<?php echo $button_text; ?>');					
-							}
-							alert(response.message);
-						}
-					});
-				}
-			});								
-		});
-	</script>
-	<?php echo apply_filters( 'em_booking_button_js', ob_get_clean(), $EM_Event ); ?>
+<?php elseif( $EM_Event->get_bookings()->get_available_spaces() <= 0 ): ?>
+	<span class="em-full-button"><?php echo $notice_full ?></span>
 <?php endif; ?>
