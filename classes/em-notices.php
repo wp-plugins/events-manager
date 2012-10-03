5f3db65e65949789206e180e7c79e7c5c262400c
netweblogic <netweblogic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-03 21:50:13 +0000

    tagging 5.2.6
    git-svn-id: https://plugins.svn.wordpress.org/events-manager/tags/5.2.6@607683 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/em-notices.php
+++ classes/em-notices.php
@@ -28,6 +28,7 @@
         }
         
         function destruct($redirect = false){
+        	//die('destructing');
         	$_SESSION['events-manager']['notices'] = serialize($this->notices);
         	return $redirect;
         }
@@ -53,32 +54,58 @@
         function add($string, $type, $static = false){
         	if( is_array($string) ){
         		$result = true;
-        		foreach($string as $string_item){
-        			if( $this->add($string_item, $type, $static) === false ){ $result = false; }
+        		foreach($string as $key => $string_item){
+        		    if( !is_array($string_item) ){
+	        			if( $this->add($string_item, $type, $static) === false ){ $result = false; }
+        			}else{
+	        			if( $this->add_item($string_item, $type, $static) === false ){ $result = false; }
+        			}
         		}
         		return $result;
         	}
             if($string != ''){
-                if( isset($this->notices[$type]) ){
-                	$notice_key = 0;
-                	foreach( $this->notices[$type] as $notice_key => $notice ){
-                		if($string == $notice['string']){
-                			return $notice_key;
-                		}
-                	}
-                    $i = $notice_key+1;
-                    $this->notices[$type][$i]['string'] = $string;
-                    if( $static ){
-                    	$this->notices[$type][$i]['static'] = true;
-                    }
-                    return $i;
-                }else{
-                    return false;
-                }
+                return $this->add_item($string, $type, $static);
             }else{
                 return false;
             }
         }
+
+        function add_item($string, $type, $static = false){
+            if( isset($this->notices[$type]) ){
+            	$notice_key = 0;
+            	foreach( $this->notices[$type] as $notice_key => $notice ){
+            		if($string == $notice['string']){
+            			return $notice_key;
+            		}elseif( is_array($string) && !empty($notice['title']) && $this->get_array_title($string) == $notice['title'] ){
+            		    return $notice_key;
+            		}
+            	}
+            	$i = $notice_key+1;
+            	if( is_array($string) ){
+            		$this->notices[$type][$i]['title'] = $this->get_array_title($string);
+					$this->notices[$type][$i]['string'] = array_shift($string);
+            	}else{
+		            $this->notices[$type][$i]['string'] = $string;
+            	}
+            	if( $static ){
+            		$this->notices[$type][$i]['static'] = true;
+            	}
+            	return $i;
+            }else{
+            	return false;
+            }
+        }
+        
+        /**
+         * Returns title of an array, assumes a assoc array with one item containing title => messages
+         * @param unknown_type $array
+         * @return unknown
+         */
+        function get_array_title($array){
+            foreach($array as $title => $msgs)
+            return $title;
+        }
+        
         function remove($key, $type){
             if( isset($this->notices[$type]) ){
                 unset($this->notices[$type][$key]);
@@ -87,19 +114,37 @@
                 return false;
             }
         }
+        
+        function remove_all(){
+        	$this->notices = array('errors'=>array(), 'infos'=>array(), 'alerts'=>array(), 'confirms'=>array());
+        }
+        
         function get($type){
             if( isset($this->notices[$type]) ){
         		$string = '';
-                foreach ($this->notices[$type] as $key => $error){
-                    $class = substr($type, 0, (strlen($type)-1));
-                    $string .= "<p>{$error['string']}</p>";
-                    if( empty($error['static']) || $error['static'] !== true){
-                        $this->remove($key, $type);
+                foreach ($this->notices[$type] as $message){
+                    if( !is_array($message['string']) ){
+	                    $string .= "<p>{$message['string']}</p>";
+                    }else{
+                        $string .= "<p><strong>".$message['title']."</strong><ul>";
+                        foreach($message['string'] as $msg){
+                            if( trim($msg) != '' ){
+                            	$string .= "<li>$msg</li>";
+                            }
+                        } 
+	                    $string .= "</ul></p>";
                     }
                 }
                 return $string;
             }
             return false;
+        }
+        
+        function count($type){
+       		if( isset($this->notices[$type]) ){
+        		return count($this->notices[$type]);
+            }
+            return 0;
         }
         
         /* Errors */
@@ -112,6 +157,9 @@
         function get_errors(){
             return $this->get('errors');
         }
+        function count_errors(){
+            return $this->count('errors');
+        }
 
         /* Alerts */
         function add_alert($string, $static=false){
@@ -122,6 +170,9 @@
         }
         function get_alerts(){
             return $this->get('alerts');
+        }
+        function count_alerts(){
+            return $this->count('alerts');
         }
         
         /* Info */
@@ -134,6 +185,9 @@
         function get_infos(){
             return $this->get('infos');
         }
+        function count_infos(){
+            return $this->count('infos');
+        }
         
         /* Confirms */
         function add_confirm($string, $static=false){
@@ -144,7 +198,10 @@
         }
         function get_confirms(){
             return $this->get('confirms');
-        }    
+        }  
+        function count_confirms(){
+            return $this->count('confirms');
+        }  
 
 		//Iterator Implementation
 	    function rewind(){
@@ -169,6 +226,9 @@
 	    }        
         
     }
-    global $EM_Notices;
-    $EM_Notices = new EM_Notices();
+    function em_notices_init(){
+	    global $EM_Notices;
+	    $EM_Notices = new EM_Notices();	
+    }
+    add_action('plugins_loaded', 'em_notices_init');
 ?>
